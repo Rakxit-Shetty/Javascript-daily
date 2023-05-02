42e0c9923c0c17233d5eed69d31c5065f42afd66
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-05-02 20:32:43 +0530

    Day 11 2nd

--- day11.js
+++ day11.js
@@ -1,6 +1,6 @@
 //1859. Sorting the Sentence
 
-/**
+/*
  * @param {string} s
  * @return {string}
  */
@@ -13,3 +13,49 @@ var sortSentence = function(s) {
     newStr.shift()
     return newStr.join(" ");
 };
+//////////////////////////////////////
+//238. Product of Array Except Self
+/*
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let res=[];
+   
+    nums.map((ele,i)=>{
+         let prod=1;
+        for(let j=0;j<nums.length;j++){
+            if(j!==i){
+                prod*=nums[j];
+            }
+        }
+      
+        res.push(prod);
+       
+        
+    })
+    return res;
+};
+
+////////////////////////////////////////////////////
+//1822. Sign of the Product of an Array
+/*
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+   let res=nums.reduce((acu,cur)=>acu*cur,1); 
+  return  signFunc(res)
+};
+
+const signFunc=(x)=>{
+if(x<0){
+    return -1;
+}
+else if(x>0){
+     return 1;
+}
+else{
+    return 0;
+}
+}
