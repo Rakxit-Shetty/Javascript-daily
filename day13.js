e7b4b9f3ea0b596994d7be92e57a4c80ca7b9cb0
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-05-04 21:20:37 +0530

    Day13

---
+++ day13.js
@@ -0,0 +1,15 @@
+//////////////////////////
+//1636. Sort Array by Increasing Frequency
+
+/*
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var frequencySort = function(nums) {
+    const map = new Map();
+  for (let n of nums) {
+      map.set(n, (map.get(n) + 1) || 1);
+  }
+  return nums.sort((a, b) => map.get(a) - map.get(b) || b - a)
+
+};
