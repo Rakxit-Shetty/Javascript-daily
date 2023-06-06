db7c0a09d0d3a551470558bf2d0078f4d4af6bc9
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-06 21:34:26 +0530

    day43-2

--- day43.js
+++ day43.js
@@ -18,3 +18,26 @@ fir++;
 }
 return true
 };
+
+//345. Reverse Vowels of a String
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    let ind=[];
+    [...s].map((ele,i)=>{
+    if(ele==="a" || ele==="e" ||ele==="i" || ele==="o" ||ele==="u" || ele==="A" || ele==="E" ||ele==="I" || ele==="O" ||ele==="U" ){
+        ind.push([ele,i]);
+    }
+    })
+    let indval=ind.map((ele)=>ele[1]).reverse()
+    let indkey=ind.map((ele)=>ele[0])
+let str=[...s];
+indval.forEach((ele,i)=>{
+str.splice(ele,1,indkey[i])
+})
+    
+    return str.join("")
+};
