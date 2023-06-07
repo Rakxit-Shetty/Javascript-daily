22afb3571cde3b01f3a9d233ca286d016eafce8e
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-07 20:47:44 +0530

    Day44-2

--- day44.js
+++ day44.js
@@ -9,3 +9,15 @@ var firstPalindrome = function(words) {
     let res= words.filter((ele)=>ele.split("").reverse().join("")==ele);
     return res[0] || ""
 };
+
+//2390. Removing Stars From a String
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    let res=[]
+s.split("").forEach(ele=>ele=="*" ? res.pop():res.push(ele))
+return res.join("")
+
+};
