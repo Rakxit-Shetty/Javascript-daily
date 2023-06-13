8a5392692cf0ae7f2059c515f2dd6d6654d21c2e
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-13 21:28:13 +0530

    day49-1.2

--- day49.js
+++ day49.js
@@ -29,3 +29,11 @@ var frequencySort = function(s) {
  }
  return res.join("")
  };
+
+ //opti
+
+ const charMap = s.split('').reduce((acc, cur) => {acc[cur] = (acc[cur] || 0) + 1; return acc} , {})
+    
+    const sortedArr = Object.keys(charMap).sort((a, b) => charMap[b] - charMap[a]);
+    
+    return sortedArr.reduce((acc, cur) => acc + cur.repeat(charMap[cur]) ,'')
