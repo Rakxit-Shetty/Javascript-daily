4433765055694f4393a08efe1aa0a1b1f5922abc
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-13 21:27:02 +0530

    day49-1

---
+++ day49.js
@@ -0,0 +1,31 @@
+//2278. Percentage of Letter in String
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+    let oclen=(s.split("").filter((ele)=>ele===letter).length) 
+    let len=s.length;
+    return Math.floor((oclen/len)*100)
+ };
+
+ //451. Sort Characters By Frequency
+ /**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    let map=new Map();
+ s.split("").forEach((ele)=>map.set(ele,(map.get(ele)||0)+1));
+ let srt=[...map.entries()].sort((a,b)=>a[1]-b[1]);
+ 
+ let res=[];
+ for(let i=0;i<srt.length;i++){
+     // console.log(srt[i][1])
+     for(let j=0;j<srt[i][1];j++){
+         res.unshift(srt[i][0])
+     }   
+ }
+ return res.join("")
+ };
