d9eff9dde2c0a0cd0b416e56fec76a9a80310459
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-19 19:23:30 +0530

    day55

---
+++ day54.js
@@ -0,0 +1,18 @@
+//1732. Find the Highest Altitude
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+
+    let res=[];
+    let fin=gain.reduce((acu,cur)=>{
+      res.push(acu)
+      return acu+=cur},0);
+      res.push(fin)
+      return Math.max(...res);
+   
+   
+   };
+
