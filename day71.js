ab7fec63c440d8c6252b5fd2ec2189445e9a4861
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-07-07 19:56:41 +0530

    day72

---
+++ day71.js
@@ -0,0 +1,9 @@
+//215: kth largest ele
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    return nums.sort((a,b)=>b-a)[k-1]
+ };
