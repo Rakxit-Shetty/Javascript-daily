b48385303da4a90049ac828412962c8ce7b328a4
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-07-18 22:04:22 +0530

    day83

---
+++ day83.js
@@ -0,0 +1,10 @@
+//561. Array Partition
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    return nums.sort((a,b)=>a-b).filter((e,i)=>i%2==0).reduce((ac,cu)=>ac+cu,0);
+};
