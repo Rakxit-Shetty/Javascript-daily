1a393f220260f3e21afd4c48b6e6a75ee6406f9e
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-07-20 22:01:52 +0530

    day85/1

--- day85.js
+++ day85.js
@@ -1,1 +1,27 @@
 //735. Asteroid Collision
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function (asteroids) {
+    let stack=[];
+    for(i=0;i<asteroids.length;i++){
+        if(stack.length==0) {
+            stack.push(asteroids[i])
+            }
+        else{
+            if(stack[stack.length-1]>0 && asteroids[i]<0){
+                if(Math.abs(stack[stack.length-1])==Math.abs( asteroids[i])){
+                    stack.pop();
+                }else if(Math.abs(stack[stack.length-1])<Math.abs(asteroids[i])){    
+                        stack.pop();
+                            i--;
+                        }
+            }
+            else{
+                stack.push(asteroids[i])
+            }
+        }
+    }
+    return stack;
+    };
