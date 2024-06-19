18c5c12f0333bbbeffc68b4c27b574fca6ab5d72
Rakxit-Shetty <rakshitshetty136@gmail.com>
2024-06-19 20:42:40 +0530

    js play with bash

--- see.sh
+++ see.sh
@@ -4,7 +4,7 @@
 commit_hashes=$(git log --format=%H -n 10 | cut -c1-7)
 
 # Set the initial date
-current_date=$(date -d "2023-08-01" +%Y-%m-%d)
+current_date=$(date -d "2024-04-01" +%Y-%m-%d)
 
 # Iterate over the commit hashes and amend the date
 for commit_hash in $commit_hashes; do
@@ -21,4 +21,19 @@ for commit_hash in $commit_hashes; do
   else
     echo "Commit $commit_hash does not exist."
   fi
+done
+
+
+start_date="2023-04-01"
+current_date=$start_date
+
+# Number of commits to create
+num_commits=108
+
+for ((i=0; i<num_commits; i++)); do
+    # Create an empty commit for the current date
+    git commit --allow-empty -m "Empty commit for $current_date" --date="$current_date 12:00:00"
+
+    # Increment the date for the next commit
+    current_date=$(date -d "$current_date + 1 day" +"%Y-%m-%d")
 done
